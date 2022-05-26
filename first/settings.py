9180a7a9bb66cbe83ff70f775dff0cd2ed32e993
evgeniypavlyukov <evgeniypavlyukov@gmail.com>
2022-05-26 12:42:19 +1100

    first_commit

--- first/settings.py
+++ first/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cms.apps.CmsConfig',
     'firstapp.apps.FirstappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -121,6 +122,15 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
